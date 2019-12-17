a5b813860ac75a3477f9b2a8efbdfb8120a5fe65
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-17 11:13:39 -0500

    Bucle Iteracion 02

--- lote.bat
+++ lote.bat
@@ -2,3 +2,8 @@ python ruiz/iteracion01.py "ABCD"
 python ruiz/iteracion01.py "ABDC"
 python ruiz/iteracion01.py "ACDD"
 python ruiz/iteracion01.py "ADAC"
+
+python ruiz/iteracion02.py "#*%$"
+python ruiz/iteracion02.py "##**"
+python ruiz/iteracion02.py "*#%%"
+python ruiz/iteracion02.py "*%$$"
---
+++ ruiz/iteracion02.py
@@ -0,0 +1,14 @@
+# EJERCICIO N° 02
+import os
+msg=os.sys.argv[1]
+for letra in msg :
+    if(letra == "#"):
+        print("Hola")
+    if(letra == "*"):
+        print("Me")
+    if(letra == "%"):
+        print("Llamo")
+    if(letra == "$"):
+        print("Naster")
+    #fin_if
+#fin_for
