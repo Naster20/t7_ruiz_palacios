51e18d7f76e440b7fa487b8e44704ed55c7b9a54
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-17 10:59:13 -0500

    Bucle Para 02

---
+++ ruiz/para02.py
@@ -0,0 +1,12 @@
+# EJERCICIO N° 02
+i = 0
+max = 50
+s = 0
+while(i < max):
+    i += 1
+    if((i % 2)!= 0):
+        print(i)
+        s = s + i
+    #fin_si
+#fin_while
+print("Suma de los impares = ",s)
