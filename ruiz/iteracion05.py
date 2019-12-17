56f41d13b40b9c576f4ac728a473a3e26251eb72
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-17 11:19:09 -0500

    Bucle Iteracion 05

---
+++ ruiz/iteracion05.py
@@ -0,0 +1,16 @@
+# EJERCICIO N° 05
+import os
+msg=os.sys.argv[1]
+for letra in msg :
+    if(letra == "F"):
+        print("Nos")
+    if(letra == "E"):
+        print("Vemos")
+    if(letra == "R"):
+        print("Mañana")
+    if(letra == "N"):
+        print("Claudia")
+    if(letra == "A"):
+        print("En el parque")
+    #fin_if
+#fin_for
