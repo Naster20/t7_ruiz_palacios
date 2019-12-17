e551e31b513bfb4d4e2a93ebadb3c5ef79dc84e6
Luis Alberto <lruizs@unprg.edu.pe>
2019-12-17 11:10:17 -0500

    Bucle Iteracion 01

---
+++ lote.bat
@@ -0,0 +1,4 @@
+python ruiz/iteracion01.py "ABCD"
+python ruiz/iteracion01.py "ABDC"
+python ruiz/iteracion01.py "ACDD"
+python ruiz/iteracion01.py "ADAC"
---
+++ ruiz/iteracion01.py
@@ -0,0 +1,14 @@
+# EJERCICIO N° 01
+import os
+msg=os.sys.argv[1]
+for letra in msg :
+    if(letra == "A"):
+        print("Hola")
+    if(letra == "B"):
+        print("Mundo")
+    if(letra == "C"):
+        print("Como")
+    if(letra == "D"):
+        print("Estas")
+    #fin_if
+#fin_for
